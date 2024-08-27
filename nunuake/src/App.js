8a5ee3e9c549128e9f845d978ba8a62ec7e593aa
bluntpencil254 <makorimike9@gmail.com>
2024-08-27 10:59:44 +0300

    deleted some files and set up a new logo

--- nunuake/src/App.js
+++ nunuake/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logos/logo3bg.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} style={{height:'350px'}}/>
+      <h2>Nunua Kenya</h2>
+        <p>A dynamic site that guarantees safety of your money when
+          you do online shopping.</p>
+        <p>Coming Soon</p>
+       
     </div>
   );
 }
